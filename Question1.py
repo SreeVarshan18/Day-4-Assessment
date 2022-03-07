f3cee47225168b10808c8431f619b463ed433f79
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-07 19:17:20 +0530

    Question 1

--- Question1.py
+++ Question1.py
@@ -1,4 +1,8 @@
-a = [4, 5, 8, 12, 13, 20]
+a = []
+n = int(input("Enter number of items in the list: "))
+for i in range(0,n):
+    y = int(input())
+    a.append(y)
 square = list(map(lambda x: x**2, a))
 cube = list(map(lambda x: x**3, a))
 print(square)
