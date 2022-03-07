7eccc80def0bee948ded3e3db030d791fa5094a1
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-07 18:59:00 +0530

    Question 1

---
+++ Question1.py
@@ -0,0 +1,5 @@
+a = [4, 5, 8, 12, 13, 20]
+square = list(map(lambda x: x**2, a))
+cube = list(map(lambda x: x**3, a))
+print(square)
+print(cube)
---
+++ Question2.py
@@ -0,0 +1,4 @@
+data = input("Enter a string: ")
+str_length = len(data)
+newfile = open("data.txt", "w")
+filedata = newfile.write(str_length)
